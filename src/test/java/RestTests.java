6c3f72cd0f59a51f0ea604c8ea09eeabff906c30
Aleksandr Anosov <you@example.com>
2024-07-18 17:19:19 +0300

    added RestTests and TestBase

---
+++ src/test/java/RestTests.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+@DisplayName("(Успешная проверка данных пользователя")
+@Tag("api")
+public class RestTests extends TestBase {
+
+}
